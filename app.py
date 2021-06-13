8660be63bd9431e078f1a13b8210be6fe1d5bb02
Nikunj <nikunj.sharma@peak.ai>
2021-06-13 11:17:45 +0530

    routes for auth flow

--- app.py
+++ app.py
@@ -1,6 +1,9 @@
 from flask import Flask
 from flask_restful import Resource, Api
 from dotenv import load_dotenv
+
+from src.deliveries.http.authenticate import Authenticate
+from src.deliveries.http.authorize import Authorize
 from src.deliveries.http.sync import Sync
 
 app = Flask(__name__)
@@ -8,13 +11,10 @@ api = Api(app)
 
 load_dotenv()
 
-class HelloWorld(Resource):
-    def get(self):
-        return {'hello': 'world'}
-
-
-api.add_resource(HelloWorld, '/')
+api.add_resource(Authorize, '/<provider>/authorize')
+api.add_resource(Authenticate, '/<provider>/authenticate')
 api.add_resource(Sync, '/sync')
+
 
 
 if __name__ == '__main__':
