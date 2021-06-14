d356f7581601029c04be9bed80c565e96140019a
Nikunj <nikunj.sharma@peak.ai>
2021-06-14 23:57:41 +0530

    added api routes for oauth and sync

--- app.py
+++ app.py
@@ -3,12 +3,12 @@ from flask_restful import Api
 from dotenv import load_dotenv
 from flask_migrate import Migrate
 from src.models import *
-from src.deliveries.http.authenticate import Authenticate
-from src.deliveries.http.authorize import Authorize
+from src.deliveries.http.authenticate_provider import AuthenticateProvider
+from src.deliveries.http.authorize_provider import AuthorizeProvider
 from src.deliveries.http.login import Login
 from src.deliveries.http.logout import Logout
 from src.deliveries.http.register import Register
-# from src.deliveries.http.sync import Sync
+from src.deliveries.http.sync_files import Sync
 load_dotenv()
 
 import os
@@ -25,9 +25,9 @@ def create_app(name):
     db.init_app(app)
     bcrypt.init_app(app)
     api = Api(app)
-    api.add_resource(Authorize, '/<provider>/authorize')
-    api.add_resource(Authenticate, '/<provider>/authenticate')
-    # api.add_resource(Sync, '/sync')
+    api.add_resource(AuthorizeProvider, '/<provider>/authorize')
+    api.add_resource(AuthenticateProvider, '/<provider>/authenticate')
+    api.add_resource(Sync, '/<provider>/sync')
     api.add_resource(Register, '/register')
     api.add_resource(Login, '/login')
     api.add_resource(Logout, '/logout')
