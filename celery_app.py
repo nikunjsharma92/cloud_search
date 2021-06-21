925439dc7d4db2a416b5015f4c1b94ab129fc4ba
Nikunj <nikunj.sharma@peak.ai>
2021-06-21 17:31:27 +0530

    refactor

--- celery_app.py
+++ celery_app.py
@@ -1,4 +1,8 @@
 from celery import Celery
 import os
+import elasticsearch_connection
 
-celery_app = Celery('celery_jobs_manager', broker=os.getenv('CELERY_BROKER') or 'redis://localhost:6379/0', include=['src.background_jobs.celery_background_tasks_wrapper'])
+config = {
+    'CELERY_ALWAY_EAGER': True
+}
+celery_app = Celery('celery_jobs_manager', broker=os.getenv('CELERY_BROKER') or 'redis://localhost:6379/0', include=['src.background_jobs.celery_background_tasks_wrapper', 'app'], config_source=config)
--- src/background_jobs/celery_background_tasks_wrapper.py
+++ src/background_jobs/celery_background_tasks_wrapper.py
@@ -1,11 +1,12 @@
 from celery_app import celery_app
 
+def add_background_job(key, *args):
+    run_background_job.delay(key, *args)
 
-def add_background_job(func, *args):
-    run_background_job.delay(func, *args)
 
-
-@celery_app.task
-def run_background_job(func, *args):
-    from src.background_jobs.job_map import background_jobs_map
-    background_jobs_map[func](*args)
+@celery_app.task(bind=True)
+def run_background_job(self, func, *args):
+    from app import app
+    with app.app_context():
+        from src.background_jobs.job_map import background_jobs_map
+        background_jobs_map[func](*args)
