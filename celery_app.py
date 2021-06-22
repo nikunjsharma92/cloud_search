dcf816c830419d6d77639137715c60ec215625a1
Nikunj <nikunj.sharma@peak.ai>
2021-06-22 19:00:23 +0530

    read broker url from env

--- celery_app.py
+++ celery_app.py
@@ -2,7 +2,5 @@ from celery import Celery
 import os
 import elasticsearch_connection
 
-config = {
-    'CELERY_ALWAY_EAGER': True
-}
-celery_app = Celery('celery_jobs_manager', broker=os.getenv('CELERY_BROKER') or 'redis://localhost:6379/0', include=['src.background_jobs.celery_background_tasks_wrapper', 'app'], config_source=config)
+config = {}
+celery_app = Celery('celery_jobs_manager', broker=os.getenv('CELERY_BROKER'), include=['src.background_jobs.celery_background_tasks_wrapper', 'app'], config_source=config)
